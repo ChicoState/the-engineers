734636e0dd4d17e2b477cbe3c840c37098764bf2
Jooms <jameskrepelka@gmail.com>
2014-09-17 00:15:23 -0700

    Added a mock-up of the login page. I'll be using this in the future for testing our database connection.

--- the3DMarket/config/routes.rb
+++ the3DMarket/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'login', to: 'login#login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
