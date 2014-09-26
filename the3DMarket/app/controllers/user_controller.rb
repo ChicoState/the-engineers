17f7d05f0d21d7022c0fa4236a9aab433811b062
Jooms <jameskrepelka@gmail.com>
2014-09-26 16:56:07 -0700

    Created new controller files.

---
+++ the3DMarket/app/controllers/user_controller.rb
@@ -0,0 +1,14 @@
+class UserController < ApplicationController
+  # User Show page
+  def show
+  end
+  # User Sign Up page
+  def create
+  end
+  # User Settings page
+  def index
+  end
+  # User login page
+  def login
+  end
+end
