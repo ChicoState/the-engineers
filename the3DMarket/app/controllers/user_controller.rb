5e5700fc75b3ecb576ebf7e6df21447dc4e71f4a
Jooms <jameskrepelka@gmail.com>
2014-10-05 23:11:06 -0700

    Added basic user creation front-end and back-end

--- the3DMarket/app/controllers/user_controller.rb
+++ the3DMarket/app/controllers/user_controller.rb
@@ -9,6 +9,43 @@ class UserController < ApplicationController
   end
   # User Sign Up page
   def create
+    
+  end
+  def try_create
+    if params[:username].present?
+      if User.find_by_username(params[:username]).present?
+        cookies[:error] = "That username is taken"
+        redirect_to(u_create_path) and return
+      end
+      if User.find_by_email(params[:email]).present?
+        cookies[:error] = "That email has already been linked to an account"
+        redirect_to(u_create_path) and return
+      end
+      if params[:password] != params[:conf_password]
+        cookies[:error] = "Your passwords don't match"
+        redirect_to(u_create_path) and return
+      end
+      if params[:password].length < 4
+        cookies[:error] = "That password is too short"
+        redirect_to(u_create_path) and return
+      end
+      
+      @user = User.new()
+      @user.username = params[:username]
+      @user.email = params[:email]
+      @user.hash_pass = PasswordHash.createHash(params[:password])
+      @user.last_log = Time.now
+      @user.save()
+
+      #Login cookie expires after an hour.
+      cookies[:user] = {:value => @user.id.to_s, :expires => Time.now + 3600}
+      
+      #Fix This
+      redirect_to(login_path)
+    else
+      cookies[:error] = "There was an error submitting, try again"
+      redirect_to(u_create_path)
+    end
   end
   # User Settings page
   def index
--- the3DMarket/config/routes.rb
+++ the3DMarket/config/routes.rb
@@ -16,7 +16,8 @@ Rails.application.routes.draw do
   ## Show
   #get 'u/:id' => 'user#show'
   ## Sign Up
-  #get 'u/create' => 'user#create'
+  get 'u/create' => 'user#create'
+  post 'u/try_create' => 'user#try_create'
   ## Settings
   #get 'u/view' => 'user#index'
   ## Login
