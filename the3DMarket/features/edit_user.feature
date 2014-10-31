51fa09f0437b78a9224879107018e25433a5677f
dleung1 <dleung1@mail.csuchico.edu>
2014-10-30 20:13:19 -0700

    testing for user edit

---
+++ the3DMarket/features/edit_user.feature
@@ -0,0 +1,38 @@
+Feature: Edit a user
+
+	Scenario: Unsuccessful Edit
+		Given a user wants to edit his information at "/u/1/view"
+		When they edit with no email
+		Then they should see the email error "Email cannot be blank"
+
+		Given a user wants to edit his information at "/u/1/view"
+		When they edit using their own email as a new email
+		Then they should see another email error "That email has already been linked to an account"
+
+		Given a user wants to edit his information at "/u/1/view"
+		When they edit using another user's email as a new email
+		Then they should see another email error "That email has already been linked to an account"
+
+		Given a user wants to edit his information at "/u/1/view"
+		When they edit their password but their confirmation password is different
+		Then they should see a password error "Your passwords don't match"
+
+		Given a user wants to edit his information at "/u/1/view"
+		When they edit their password but there is no confirmation password
+		Then they should see a password error "Your passwords don't match"
+
+		Given a user wants to edit his information at "/u/1/view"
+		When they edit their password but there is no password
+		Then they should see a password error "Your passwords don't match"
+
+		Given a user wants to edit his information at "/u/1/view"
+		When they edit their password but their password and confirmation password do not have more than 3 characters
+		Then they should see another password error "That password is too short"
+
+	Scenario: Successful Edit
+
+		Given a user wants to edit his information at "/u/1/view"
+		When they edit with a valid email, password, and confirmation password
+		Then they should be redirected to their user account page
+
+
---
+++ the3DMarket/features/step_definitions/edit_user.rb
@@ -0,0 +1,80 @@
+Given /^a user wants to edit his information at "([^"]*)"$/ do |user_edit_page|
+	visit user_edit_page
+end
+
+#Test with invalid information
+When /^they edit with no email$/ do
+	fill_in "password",		with: "1234"
+	fill_in "confirm_password",	with: "1234"
+	click_button "Save Changes!"
+end
+
+When /^they edit using their own email as a new email$/ do
+	fill_in "email",		with: "jkrepelka@mail.csuchico.edu"
+	fill_in "password",		with: "1234"
+	fill_in "confirm_password",	with: "1234"
+	click_button "Save Changes!"
+end
+
+When /^they edit using another user's email as a new email$/ do
+	fill_in "email",		with: "jkrepelka@mail.csuchico.edu"
+	fill_in "password",		with: "1234"
+	fill_in "confirm_password",	with: "1234"
+	click_button "Save Changes!"
+end
+
+When /^they edit their password but their confirmation password is different$/ do
+	fill_in "email",		with: "hello@mail.csuchico.edu"
+	fill_in "password",		with: "1234"
+	fill_in "confirm_password",	with: "4321"
+	click_button "Save Changes!"
+end
+
+When /^they edit their password but there is no confirmation password$/ do
+	fill_in "email",		with: "hello@mail.csuchico.edu"
+	fill_in "password",		with: "1234"
+	click_button "Save Changes!"
+end
+
+When /^they edit their password but there is no password$/ do
+	fill_in "email",		with: "hello@mail.csuchico.edu"
+	fill_in "confirm_password",		with: "1234"
+	click_button "Save Changes!"
+end
+
+When /^they edit their password but their password and confirmation password do not have more than 3 characters$/ do
+	fill_in "email",		with: "hello@mail.csuchico.edu"
+	fill_in "password",		with: "123"
+	fill_in "confirm_password",	with: "123"
+	click_button "Save Changes!"
+end
+
+#Test with valid information
+When /^they edit with a valid email, password, and confirmation password$/ do
+	fill_in "email",		with: "hello@mail.csuchico.edu"
+	fill_in "password",		with: "1234"
+	fill_in "confirm_password",	with: "1234"
+	click_button "Save Changes!"
+end
+
+#Error messages
+Then /^they should see the email error "([^"]*)"$/ do |email_error|
+	page.should have_content email_error
+end
+
+Then /^they should see another email error "([^"]*)"$/ do |another_email_error|
+	page.should have_content another_email_error
+end
+
+Then /^they should see a password error "([^"]*)"$/ do |password_error|
+	page.should have_content password_error
+end
+
+Then /^they should see another password error "([^"]*)"$/ do |another_password_error|
+	page.should have_content another_password_error
+end
+
+#Success
+Then /^they should be redirected to their user account page$/ do
+	page.should have_content "test"
+end
