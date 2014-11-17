05d98080adadbf0adecca75da560e247bfcbf730
Jooms <jameskrepelka@gmail.com>
2014-11-16 22:12:47 -0800

    Altered Front Page to use recently uploaded designs.

--- the3DMarket/app/controllers/main_controller.rb
+++ the3DMarket/app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 class MainController < ApplicationController
   # The Front page
   def index
+    @designs = Design.order('created_at DESC').limit(10).to_a
+    @main_feature = @designs.last
+    @minor_feature = @designs - [@main_feature]
   end
   # The About page
   def about
