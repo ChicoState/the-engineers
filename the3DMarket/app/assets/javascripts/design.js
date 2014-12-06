cdb832e9ea78a56c11a9866e5a4231350f7c5097
Jooms <jameskrepelka@gmail.com>
2014-12-05 18:00:28 -0800

    More changes with the design rotation file to test on the server

--- the3DMarket/app/assets/javascripts/design.js
+++ the3DMarket/app/assets/javascripts/design.js
@@ -2,11 +2,11 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var camera, cameraTarget, controls, scene, renderer;
 
-function init () {
+function init() {
 
   camera = new THREE.PerspectiveCamera( 30, 640 / 480, 1, 1000 );
   camera.position.set( 2, 1, 2 );
-  
+
   controls = new THREE.TrackballControls( camera );
 
   controls.rotateSpeed = 5.0;
@@ -18,7 +18,7 @@ function init () {
 
   controls.staticMoving = true;
   controls.dynamicDampingFactor = 1;
-  
+
   cameraTarget = new THREE.Vector3( 0, 0.25, 0 );
 
   scene = new THREE.Scene();
@@ -50,11 +50,10 @@ function init () {
 
 	  mesh.position.set( 0, 0, 0 );
 	  mesh.rotation.set( -Math.PI/2 , 0, 0);
-	  mesh.scale.set( 1.0/(2*a.x), 1.0/(2*a.x), 1.0/(2*a.x) );
-	  //mesh.scale.set( 1.0/(2*1), 1.0/(2*1), 1.0/(2*1) );
+	  mesh.scale.set( 1/(2*a.x), 1/(2*a.x), 1/(2*a.x) );
 	  mesh.castShadow = true;
 	  mesh.receiveShadow = true;
-    alert (box.size().x)
+
 	  scene.add( mesh );
 
   } );
@@ -82,38 +81,52 @@ function init () {
   renderer.shadowMapCullFace = THREE.CullFaceBack;
 
   $('.stl-scene').append( renderer.domElement );
+
 }
-//this defines the shadowedlight function above.
-function addShadowedLight(x, y, z, color, intensity) {
-  var directionalLight = new THREE.DirectionalLight(color, intensity);
-  directionalLight.position.set(x, y, z);
-  scene.add(directionalLight);
+
+function addShadowedLight( x, y, z, color, intensity ) {
+
+  var directionalLight = new THREE.DirectionalLight( color, intensity );
+  directionalLight.position.set( x, y, z )
+  scene.add( directionalLight );
+
   directionalLight.castShadow = true;
-  // directionalLight.shadowCameraVisible = true;
+  //directionalLight.shadowCameraVisible = true;
+
   var d = 1;
   directionalLight.shadowCameraLeft = -d;
   directionalLight.shadowCameraRight = d;
   directionalLight.shadowCameraTop = d;
   directionalLight.shadowCameraBottom = -d;
+
   directionalLight.shadowCameraNear = 1;
   directionalLight.shadowCameraFar = 4;
+
   directionalLight.shadowMapWidth = 1024;
   directionalLight.shadowMapHeight = 1024;
+
   directionalLight.shadowBias = -0.005;
-  directionalLight.shadowDarkness = 0.15;
+  directionalLight.shadowDarkness = 0.2;
+
 }
 
 function animate() {
+
   requestAnimationFrame( animate );
-	//controls.update();
-	render();
+  controls.update();
+  render();
+
 }
 
 function render() {
-  //this handles rotation
+
   //var timer = Date.now() * 0.0005;
-  //camera.position.x = Math.cos(timer) * 3;
-  //camera.position.z = Math.sin(timer) * 3;
-  camera.lookAt(cameraTarget);
-  renderer.render(scene, camera);
+
+  //camera.position.x = Math.cos( timer ) * 3;
+  //camera.position.z = Math.sin( timer ) * 3;
+
+  camera.lookAt( cameraTarget );
+
+  renderer.render( scene, camera );
+
 }
